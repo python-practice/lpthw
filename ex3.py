c5be701e536add3954b94c4e199bfb127a54ead5
Sergey Vlasov <im.sergey.vlasov@gmail.com>
2015-03-19 14:26:07 +0200

    ex #3, dd #1

--- ex3.py
+++ ex3.py
@@ -1,23 +1,33 @@
+# print a plain string
 print "I will now count my chickens:"
 
+# concatenate string with number and print out
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+# print a string
 print "Now I will count the eggs:"
 
+# print a number
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# print a question
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# print the answer
 print 3 + 2 < 5 - 7
 
+# print a question and the answer as a single line
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# print another plain string
 print "Oh, that's why it's False."
 
+# print a plain string once more
 print "How about some more."
 
+# another example of a string and number concatenation
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2 
