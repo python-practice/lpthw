5d000139c7bc46391d9003be8716ea8328ea2b81
Sergey Vlasov <im.sergey.vlasov@gmail.com>
2015-03-19 17:19:57 +0200

    ex 5 sd 4

--- ex5.py
+++ ex5.py
@@ -1,7 +1,15 @@
+INCH_TO_CM = 2.54
+POUND_TO_KG = 0.453592
+
 my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
+
 my_height = 74 # inches
+my_height_cm = my_height * INCH_TO_CM # centimeters
+
 my_weight = 180 # lbs
+my_weight_kg = my_weight * POUND_TO_KG # kilograms
+
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
@@ -16,3 +24,6 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d and %d I get %d." % (
 	my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "He's %d cm tall." % my_height_cm
+print "He's %d kg heavy." % my_weight_kg
